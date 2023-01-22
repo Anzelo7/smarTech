1fa5cf701ecdf47cd49d0999ee7639af6f3aa9f0
Anzelo7 <andrea.anzellotti7@gmail.com>
2023-01-22 23:08:56 +0100

    add Footer.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbars from './components/Navbar.js';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbars />
       <Header />
       <Main />
+      <Footer />
     </div>
 
   );
