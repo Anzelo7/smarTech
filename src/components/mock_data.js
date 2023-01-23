24dae2b3b0533c4341c6403ced5df97ce650f78b
Anzelo7 <andrea.anzellotti7@gmail.com>
2023-01-23 16:42:37 +0100

    dbg

--- src/components/mock_data.js
+++ src/components/mock_data.js
@@ -1,98 +1,98 @@
 export const data = [
     {
-        image:"src/img/seller/avatar1.png",
+        image: "./img/seller/avatar1.png",
         full_name: "Siouxie Backstar",
         title: "Career Coach",
         email: "sbaxstar5@gmail.com",
         phone: "+39 837-9786",
-        job:"L'assistenza al cliente è il primo passo per un'azienda sana, la mia missione è seguire chi cerca un impiego nel mondo del digitale, accompagnandolo verso un inzio smart!."
+        job: "L'assistenza al cliente è il primo passo per un'azienda sana, la mia missione è seguire chi cerca un impiego nel mondo del digitale, accompagnandolo verso un inzio smart!."
     },
     {
-        image:"src/img/seller/avatar2.png",
+        image: "./img/seller/avatar2.png",
         full_name: "Ruy Blonden",
         title: "Ingegnere Nucleare",
         email: "rblonden1@gmail.com",
         phone: "+39 796-4960",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar3.png",
+        image: "./img/seller/avatar3.png",
         full_name: "Artur Nobriga",
         title: "Assistente Ingegnere Edile",
         email: "artbriga2@gmail.com",
         phone: "+39 469-4829",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar4.png",
+        image: "./img/seller/avatar4.png",
         full_name: "Pierrette Cheavin",
         title: "Web Developer",
         email: "pcheavin3@gmail.com",
         phone: "+39 845-7544",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar5.png",
+        image: "./img/seller/avatar5.png",
         full_name: "Skip Camerati",
         title: "Editore",
         email: "scamerati4@gmail.como",
         phone: "+39 806-1969",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar6.png",
+        image: "./img/seller/avatar6.png",
         full_name: "Drona Corn",
         title: "Software Developer",
         email: "dcorn5@gmail.com",
         phone: "+39 699-8196",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam." 
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar7.png",
+        image: "./img/seller/avatar7.png",
         full_name: "Tejn Jeanequin",
         title: "Tecnico Idraulico",
         email: "tjeanequin6@gmail.com",
         phone: "+39 342-2775",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar8.png",
+        image: "./img/seller/avatar8.png",
         full_name: "Des MacAn",
         title: "Ingegnere Agricolo",
         email: "dmacan7@gmail.com",
         phone: "+39 793-1269",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar9.png",
+        image: "./img/seller/avatar9.png",
         full_name: "Danila Fantone",
         title: "Chief Risorse Umane",
         email: "dfantone8@gmail.com",
         phone: "+39 406-7863",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar10.png",
+        image: "./img/seller/avatar10.png",
         full_name: "Elfie Joutapaitis",
         title: "Commercialista",
         email: "ejoutapaitis9@gmail.com",
         phone: "+39 437-4602",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar11.png",
+        image: "./img/seller/avatar11.png",
         full_name: "Pauline Ashmole",
         title: "Avvocato",
         email: "pashmolea@gmail.com",
         phone: "+39 436-2287",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     },
     {
-        image:"src/img/seller/avatar12.png",
+        image: "./img/seller/avatar12.png",
         full_name: "Derry Durie",
         title: "Sviluppatore di Sistemi",
         email: "durieD@gmail.com",
         phone: "+39 197-7023",
-        job:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
+        job: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe animi cum quisquam minus, magni at incidunt illum modi tenetur, assumenda aspernatur consectetur sapiente quibusdam earum reprehenderit provident dolore harum aperiam."
     }
 ]
