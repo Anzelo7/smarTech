df82c0d8b8920eeaa2f4a6b2d6500f2d29e164a4
Anzelo7 <andrea.anzellotti7@gmail.com>
2023-01-23 11:43:21 +0100

    modf

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
     return (
 
         <Card.Footer className='d-flex justify-content-between bg-light-dark mt-4' style={{ backgroundColor: '#282c34' }}>
-            <div>
+            <Nav.Link className='mt-1'>
                 <i className="bi bi-facebook fs-2 mx-3" style={{ color: "white" }}></i>
-                <i className="bi bi-twitter fs-2 mx-2" style={{ color: "white" }}></i>
-                <i className="bi bi-discord fs-2 mx-2" style={{ color: "white" }}></i>
-            </div>
-            <div href="#" className='mt-1'>
+                <i className="bi bi-twitter fs-2" style={{ color: "white" }}></i>
+                <i className="bi bi-discord fs-2 mx-3" style={{ color: "white" }}></i>
+            </Nav.Link>
+            <Nav.Link href="#" className='mt-1'>
                 <img
                     src={Logo}
                     width=""
@@ -19,7 +19,7 @@ function Footer() {
                     className=""
                     alt=""
                 />
-            </div>
+            </Nav.Link>
             <div>
                 <Nav.Item>
                     <Nav.Link href="/home" className="fs-5 mt-4 mx-2" style={{ color: "white" }}>smarTech@business.org</Nav.Link>
